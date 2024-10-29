2a860c9ee9fd66b8f90f9a9bf1d7bc87b263fd68
ryand939 <ryandaer@gmail.com>
2024-10-29 13:24:39 -0400

    Regen API key and now use env variables... I'm uncertain whether apikey for this project should be public as my rules allow for unauthed users to READ only. Keeping it secret now though

--- src/firebase.js
+++ src/firebase.js
@@ -6,22 +6,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyDyaKkE5ZLxSckYSgn_95mxTAn9Z9oPXns",
-  
-    authDomain: "daerbot-dashboard.firebaseapp.com",
-  
-    projectId: "daerbot-dashboard",
-  
-    storageBucket: "daerbot-dashboard.appspot.com",
-  
-    messagingSenderId: "219144130030",
-  
-    appId: "1:219144130030:web:5c741bfe66b28ff37912ff",
-  
-    measurementId: "G-J5291PX7V2"
-  
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
   
   
 const app = initializeApp(firebaseConfig);
